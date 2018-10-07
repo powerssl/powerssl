d7454806caecd8953dc3d2703217fc9ccea401db
Daniel Spangenberg <daniel@spangenberg.io>
2018-10-07 19:05:18 +0200

    Rename and regenerate resources

--- pkg/client/grpc_client.go
+++ pkg/client/grpc_client.go
@@ -4,16 +4,24 @@ import (
 	"github.com/go-kit/kit/log"
 	"google.golang.org/grpc"
 
-	certificateauthorityservice "powerssl.io/pkg/resources/certificate_authority/service"
-	certificateauthoritytransport "powerssl.io/pkg/resources/certificate_authority/transport"
+	certificateservice "powerssl.io/pkg/gen/certificate/service"
+	certificatetransport "powerssl.io/pkg/gen/certificate/transport"
+	certificateauthorityservice "powerssl.io/pkg/gen/certificateauthority/service"
+	certificateauthoritytransport "powerssl.io/pkg/gen/certificateauthority/transport"
+	certificateissueservice "powerssl.io/pkg/gen/certificateissue/service"
+	certificateissuetransport "powerssl.io/pkg/gen/certificateissue/transport"
 )
 
 type grpcClient struct {
+	Certificate          certificateservice.Service
 	CertificateAuthority certificateauthorityservice.Service
+	CertificateIssue     certificateissueservice.Service
 }
 
 func NewGRPCClient(conn *grpc.ClientConn, logger log.Logger) *grpcClient {
 	return &grpcClient{
+		Certificate:          certificatetransport.NewGRPCClient(conn, logger),
 		CertificateAuthority: certificateauthoritytransport.NewGRPCClient(conn, logger),
+		CertificateIssue:     certificateissuetransport.NewGRPCClient(conn, logger),
 	}
 }
