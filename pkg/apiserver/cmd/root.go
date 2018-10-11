c0c555b46b92c974c5a603afcf23ab689dfbb84e
Daniel Spangenberg <daniel@spangenberg.io>
2018-10-12 01:51:48 +0200

    Cleanup cobra

--- pkg/apiserver/cmd/root.go
+++ pkg/apiserver/cmd/root.go
@@ -8,7 +8,10 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	Verbose bool
+	cfgFile string
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -17,8 +20,6 @@ var rootCmd = &cobra.Command{
 	Long: `powerssl-apiserver provides PowerSSL API.
 
 Find more information at: https://powerssl.io`,
-	// Run: func(cmd *cobra.Command, args []string) {
-	// },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -33,14 +34,8 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is /etc/powerssl/api/config.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Verbose output")
 }
 
 // initConfig reads in config file and ENV variables if set.
--- pkg/powerctl/cmd/root.go
+++ pkg/powerctl/cmd/root.go
@@ -9,7 +9,10 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	Verbose bool
+	cfgFile string
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -32,14 +35,8 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.powerctl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Verbose output")
 }
 
 // initConfig reads in config file and ENV variables if set.
