eb8f04a3424979896ec529413a793c23bcb8ccf7
Daniel Spangenberg <daniel@spangenberg.io>
2018-10-15 02:04:09 +0200

    Serializers for resources

--- pkg/api/certificate_types.go
+++ pkg/api/certificate_types.go
@@ -3,16 +3,16 @@ package api // import "powerssl.io/pkg/api"
 import "time"
 
 type Certificate struct {
-	Name            string
-	CreateTime      time.Time
-	UpdateTime      time.Time
-	DisplayName     string
-	Title           string
-	Description     string
-	Labels          map[string]string
-	Dnsnames        []string
-	KeyAlgorithm    string
-	KeySize         int32
-	DigestAlgorithm string
-	AutoRenew       bool
+	Name            string            `json:"name,omitempty"            yaml:"name,omitempty"`
+	CreateTime      time.Time         `json:"createTime,omitempty"      yaml:"createTime,omitempty"`
+	UpdateTime      time.Time         `json:"updateTime,omitempty"      yaml:"updateTime,omitempty"`
+	DisplayName     string            `json:"displayName,omitempty"     yaml:"displayName,omitempty"`
+	Title           string            `json:"title,omitempty"           yaml:"title,omitempty"`
+	Description     string            `json:"description,omitempty"     yaml:"description,omitempty"`
+	Labels          map[string]string `json:"labels,omitempty"          yaml:"labels,omitempty"`
+	Dnsnames        []string          `json:"dnsnames,omitempty"        yaml:"dnsnames,omitempty"`
+	KeyAlgorithm    string            `json:"keyAlgorithm,omitempty"    yaml:"keyAlgorithm,omitempty"`
+	KeySize         int32             `json:"keySize,omitempty"         yaml:"keySize,omitempty"`
+	DigestAlgorithm string            `json:"digestAlgorithm,omitempty" yaml:"digestAlgorithm,omitempty"`
+	AutoRenew       bool              `json:"autoRenew,omitempty"       yaml:"autoRenew,omitempty"`
 }
