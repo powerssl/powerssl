52119ddfa8502040a9ef9d235da7b2c510a0fb9e
Daniel Spangenberg <daniel@spangenberg.io>
2018-11-18 18:28:50 +0100

    Split workflows in their own files

---
+++ pkg/controller/workflow/engine/workflow/request_certificate.go
@@ -0,0 +1,40 @@
+package workflow
+
+import (
+	"context"
+
+	"powerssl.io/pkg/controller/api"
+	"powerssl.io/pkg/controller/workflow/engine/activity"
+	"powerssl.io/pkg/controller/workflow/engine/activity/acme"
+)
+
+type RequestCertificate struct {
+	DirectoryURL string
+	AccountURL   string
+	Dnsnames     []string
+	NotBefore    string
+	NotAfter     string
+}
+
+func (w RequestCertificate) Kind() string {
+	return "RequestCertificate"
+}
+
+func (w RequestCertificate) Run(ctx context.Context) {
+	identifiers := make([]*api.Identifier, len(w.Dnsnames))
+	for i, dnsname := range w.Dnsnames {
+		identifiers[i] = &api.Identifier{Type: api.IdentifierTypeDNS, Value: dnsname}
+	}
+
+	{
+		a := activity.New(api.Activity_ACME_CREATE_ORDER)
+		a.SetInput(acme.CreateOrderInput{w.DirectoryURL, w.AccountURL, identifiers, w.NotBefore, w.NotAfter})
+		a.Execute(ctx)
+	}
+	for range []struct{}{} {
+		activity.New(api.Activity_ACME_GET_AUTHORIZATION)
+		activity.New(api.Activity_ACME_VALIDATE_CHALLENGE)
+	}
+	activity.New(api.Activity_ACME_FINALIZE_ORDER)
+	activity.New(api.Activity_ACME_GET_CERTIFICATE)
+}
--- pkg/controller/workflow/engine/workflow/workflow.go
+++ pkg/controller/workflow/engine/workflow/workflow.go
@@ -7,10 +7,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/opentracing/opentracing-go"
 
-	apiserverapi "powerssl.io/pkg/apiserver/api"
 	"powerssl.io/pkg/controller/api"
-	"powerssl.io/pkg/controller/workflow/engine/activity"
-	"powerssl.io/pkg/controller/workflow/engine/activity/acme"
 )
 
 type Definition interface {
@@ -61,81 +58,4 @@ func (w *Workflow) execute(ctx context.Context, c chan struct{}) {
 	// 	fmt.Println(ctx.Err()) // TODO
 	// }
 	close(c)
-}
-
-type CreateAccount struct {
-	AccountName          string
-	DirectoryURL         string
-	TermsOfServiceAgreed bool
-	Contacts             []string
-}
-
-func (w CreateAccount) Kind() string {
-	return "CreateAccount"
-}
-
-func (w CreateAccount) Run(ctx context.Context) {
-	createAccountResult, err := w.CreateAccount(ctx, w.DirectoryURL, w.TermsOfServiceAgreed, w.Contacts)
-	if err != nil {
-		panic(err)
-	}
-	if createAccountResult.Error != nil {
-		panic(err)
-	}
-	acmeAccount := &apiserverapi.ACMEAccount{
-		AccountURL: createAccountResult.Account.URL,
-		// Contacts:             createAccountResult.Account.Contacts,
-		// Status:               createAccountResult.Account.Status,
-		// TermsOfServiceAgreed: createAccountResult.Account.TermsOfServiceAgreed,
-	}
-	var _ = acmeAccount
-	//if _, err := client.ACMEAccount.Update(context.Background(), w.AccountName, acmeAccount); err != nil {
-	//	panic(err)
-	//}
-}
-
-func (w CreateAccount) CreateAccount(ctx context.Context, directoryURL string, termsOfServiceAgreed bool, contacts []string) (*acme.CreateAccountResult, error) {
-	a := activity.NewV2(acme.CreateAccount{})
-	a.SetInput(acme.CreateAccountInput{
-		DirectoryURL:         directoryURL,
-		TermsOfServiceAgreed: termsOfServiceAgreed,
-		Contacts:             contacts,
-	})
-	<-a.Execute(ctx)
-	var result *acme.CreateAccountResult
-	if err := a.GetResult(&result); err != nil {
-		return nil, err
-	}
-	return result, nil
-}
-
-type RequestCertificate struct {
-	DirectoryURL string
-	AccountURL   string
-	Dnsnames     []string
-	NotBefore    string
-	NotAfter     string
-}
-
-func (w RequestCertificate) Kind() string {
-	return "RequestCertificate"
-}
-
-func (w RequestCertificate) Run(ctx context.Context) {
-	identifiers := make([]*api.Identifier, len(w.Dnsnames))
-	for i, dnsname := range w.Dnsnames {
-		identifiers[i] = &api.Identifier{Type: api.IdentifierTypeDNS, Value: dnsname}
-	}
-
-	{
-		a := activity.New(api.Activity_ACME_CREATE_ORDER)
-		a.SetInput(acme.CreateOrderInput{w.DirectoryURL, w.AccountURL, identifiers, w.NotBefore, w.NotAfter})
-		a.Execute(ctx)
-	}
-	for range []struct{}{} {
-		activity.New(api.Activity_ACME_GET_AUTHORIZATION)
-		activity.New(api.Activity_ACME_VALIDATE_CHALLENGE)
-	}
-	activity.New(api.Activity_ACME_FINALIZE_ORDER)
-	activity.New(api.Activity_ACME_GET_CERTIFICATE)
 }
