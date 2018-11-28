35016804beb21d19479615209e868799f70fe709
Daniel Spangenberg <daniel@spangenberg.io>
2018-11-28 01:31:54 +0100

    Regenerate docs with yaml information

--- cmd/powerssl-integration-cloudflare-doc/main.go
+++ cmd/powerssl-integration-cloudflare-doc/main.go
@@ -3,15 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra/doc"
+	"gopkg.in/yaml.v2"
 
 	"powerssl.io/pkg/integrations/cloudflare/cmd"
 )
 
 func main() {
-	if err := doc.GenMarkdownTree(cmd.RootCmd(), "docs/powerssl-integration-cloudflare"); err != nil {
+	if err := doc.GenMarkdownTreeCustom(cmd.RootCmd(), "docs/powerssl-integration-cloudflare", filePrepender, linkHandler); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func filePrepender(filename string) string {
+	name := filepath.Base(filename)
+	base := strings.TrimSuffix(name, path.Ext(name))
+	s := strings.Split(base, "_")
+
+	meta := make(map[string]interface{})
+	meta["layout"] = "default"
+	meta["title"] = s[len(s)-1]
+	switch len(s) {
+	case 1:
+		meta["has_children"] = true
+	case 2:
+		meta["parent"] = s[len(s)-2]
+	}
+	byt, _ := yaml.Marshal(meta)
+	return fmt.Sprintf("---\n%s---\n", string(byt))
+}
+
+func linkHandler(name string) string {
+	return name
 }
