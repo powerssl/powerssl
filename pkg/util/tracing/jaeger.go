d3f1a696b2efe8f61e476747dfac3a9833a97503
Daniel Spangenberg <daniel@spangenberg.io>
2019-01-27 18:23:42 +0100

    Add jeager environment config

--- pkg/util/tracing/jaeger.go
+++ pkg/util/tracing/jaeger.go
@@ -5,21 +5,15 @@ import (
 
 	"github.com/go-kit/kit/log"
 	"github.com/opentracing/opentracing-go"
-	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	jaegerlog "github.com/uber/jaeger-lib/client/log/go-kit"
 	jaegerprometheus "github.com/uber/jaeger-lib/metrics/prometheus"
 )
 
 func NewJaegerTracer(serviceName string, logger log.Logger) (opentracing.Tracer, io.Closer, error) {
-	cfg := jaegercfg.Configuration{
-		Sampler: &jaegercfg.SamplerConfig{
-			Type:  jaeger.SamplerTypeConst,
-			Param: 1,
-		},
-		Reporter: &jaegercfg.ReporterConfig{
-			LogSpans: true,
-		},
+	cfg, err := jaegercfg.FromEnv()
+	if err != nil {
+		return nil, nil, err
 	}
 
 	jaegerLogger := jaegerlog.NewLogger(logger)
