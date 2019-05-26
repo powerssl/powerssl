c50dfb018d15d10c8dc62827b5fea8d82c6b0238
Daniel Spangenberg <daniel@spangenberg.io>
2019-05-26 20:59:37 +0200

    Refactor build scripts

--- scripts/build-go.sh
+++ scripts/build-go.sh
@@ -1,22 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-set -eux
+set -euxo pipefail
 
-FORCE_REBUILD_ARG=''
-if [ -n "${FORCE_REBUILD:-}" ]; then
-  FORCE_REBUILD_ARG='-a'
-fi
-
-EXTLDFLAGS='-static'
-EXT=''
-
-if [ -z "${GOOS:-}" ]; then
-  case "${OSTYPE:-}" in
-    darwin*) EXTLDFLAGS='' ;;
-  esac
-elif [ "${GOOS}" == 'windows' ]; then
-  EXT='.exe'
-fi
-
-CGO_ENABLED="${CGO_ENABLED:-0}"
-go build ${FORCE_REBUILD_ARG} -tags netgo -ldflags "-w -extldflags \"${EXTLDFLAGS}\"" -o "bin/${COMPONENT}${EXT}" "powerssl.io/powerssl/cmd/${COMPONENT}"
+env CGO_ENABLED="${CGO_ENABLED:-0}" go build ${FORCE_REBUILD:+-a} -tags netgo -ldflags "-w -extldflags \"-static\" $(govvv build -flags -pkg powerssl.io/powerssl/internal/pkg/version)" -o "bin/$COMPONENT" "powerssl.io/powerssl/cmd/$COMPONENT"
