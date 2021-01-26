5b243e56bb6e87b06189e2ccfb64ca549d9780c4
Daniel Spangenberg <daniel@spangenberg.io>
2021-01-26 16:38:34 +0100

    Fix migrate script

--- scripts/migrate-create.sh
+++ scripts/migrate-create.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 
-gobin -m -run github.com/golang-migrate/migrate/v4/cmd/migrate create -ext sql -dir db/migrations $@
+gobin -m -run github.com/golang-migrate/migrate/v4/cmd/migrate create -ext sql -dir db/migrations "$@"
