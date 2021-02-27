c1431464f3fad2760d5052f9b77e00b526c41fbf
Daniel Spangenberg <daniel@spangenberg.io>
2021-02-27 15:39:16 +0100

    Tweak scripts

--- apiserver/internal/migration/doc.go
+++ apiserver/internal/migration/doc.go
@@ -1,3 +1,3 @@
-//go:generate gobin -m -run github.com/go-bindata/go-bindata/go-bindata -fs -modtime 726710400 -pkg asset -prefix ../../db/migrations ../../db/migrations
+//go:generate gobin -m -run github.com/go-bindata/go-bindata/go-bindata -fs -modtime 726710400 -pkg migration -prefix ../../db/migrations ../../db/migrations
 
 package migration
